d28726d26432f543a2479ba4aa2ad4e212218818
sdolgov <sdolgov@rbbn.com>
2022-08-13 21:35:20 +0300

    modified expenses list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import CostItem from "./components/CostItem";
+import ExpensesList from "./components/ExpensesList"
 
 function App() {
-  return (
-      <div>
-        <h1>Let's start studying!</h1>
-      </div>
-  );
+    const expenses = [
+        {
+            date: new Date(2022, 8, 15),
+            name: 'Ref',
+            cost: 999.99
+        },
+        {
+            date: new Date(2022, 7, 1),
+            name: 'Vacuum cleaner',
+            cost: 199.99
+        },
+        {
+            date: new Date(2022, 7, 7),
+            name: 'Cellphone',
+            cost: 599.99
+        }
+    ]
+
+    return (
+        <div>
+            <h1>Let's start studying!</h1>
+            <ExpensesList expenses={expenses}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/PurchaseDate.js
@@ -0,0 +1,17 @@
+import './PurcahseDate.css'
+
+function PurchaseDate(props) {
+    const month = props.date.toLocaleString('default', {month: 'long'})
+    const year = props.date.getFullYear()
+    const day = props.date.toLocaleString('default', {day: '2-digit'})
+
+    return (
+        <div className='cost-date'>
+            <div className='cost-date__month'>{month}</div>
+            <div className='cost-date__year'>{year}</div>
+            <div className='cost-date__day'>{day}</div>
+        </div>
+    )
+}
+
+export default PurchaseDate
